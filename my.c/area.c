0d339cad8cd5a204582080e451e36bd5e71aa8c9
eminentetim <etimeminent@gmail.com>
2023-05-21 18:07:16 -0400

    creating directories for my C projects

---
+++ my.c/area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int a, s;
+printf("Enter length of side: ");
+/** store the length in s **/
+scanf("%i", &s);
+/* calculate area ; store in a */
+a = s * s;
+printf ("\nArea of square is %i\n", a);
+} 
+
