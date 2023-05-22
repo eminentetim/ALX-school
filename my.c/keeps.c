932799c40c02ab1aa6fc0888a0d3f257c409dae2
eminentetim <etimeminent@gmail.com>
2023-05-21 21:26:55 -0400

    test3

---
+++ my.c/keeps.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("May the good God\n");
+	printf("Bless and keep you\n");
+	printf("either far nor near\n");
+	printf("May his face shine upon you\n");
+}
---
+++ my.c/test3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	int a, b;
+	a = 30;
+	b = 4;
+	printf("The product of %d and %d is %d\n", a, b, a * b);
+	printf("%d + %d = %d\n", a, b, a + b);
+	printf("%d - %d = %d\n", a, b, a - b);
+	printf("%d x %d = %d\n", a, b, a * b);
+}
