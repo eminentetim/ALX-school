15ec52648e0abeab3a4f9e7b60196e1741540718
eminentetim <etimeminent@gmail.com>
2023-05-23 06:45:16 -0400

    nest-while loop function

---
+++ c-basics/nest-wloop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* main - excuting while loop successfuly */
+int main(void)
+{
+int i = 0, j;
+
+while (i < 5)
+{
+
+	j = 0;
+	while (j < 3)
+	{
+		printf("2 ");
+		j++;
+
+	}
+	printf("\n");
+	i++;
+}
+	return (0);
+}
